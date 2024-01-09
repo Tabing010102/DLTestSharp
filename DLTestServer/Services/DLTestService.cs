b52d7ab0dfddc62db70f7d3af3edd655f42b0b5b
Tabing010102 <tabing010102@outlook.com>
2024-01-09 11:04:39 +0800

    vs init

---
+++ DLTestServer/Services/DLTestService.cs
@@ -0,0 +1,19 @@
+﻿using DLTest;
+using Grpc.Core;
+
+namespace DLTestServer.Services
+{
+    public class DLTestService : DLTest.DLTest.DLTestBase
+    {
+        private readonly ILogger<DLTestService> _logger;
+        public DLTestService(ILogger<DLTestService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<GetDLResponse> GetDLFiles(GetDLRequest request, ServerCallContext context)
+        {
+            return base.GetDLFiles(request, context);
+        }
+    }
+}
